7c00a88f0e710e530573d5ebb636ad8c2dd3041c
Keshav4120 <08keshavjindal10@gmail.com>
2025-10-25 03:46:23 +0000

    Completed

--- src/assets/JS/index.js
+++ src/assets/JS/index.js
@@ -1,3 +1,7 @@
-function toggleMenu() {
-    document.getElementById("navbar").classList.toggle("show");
-  }
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
+
+    hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("show");
+      hamburger.classList.toggle("open");
+    });
